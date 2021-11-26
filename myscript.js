eb0812041f48e9aa29bc1c0e70552e4a3208d058
Willibomb <94479851+Willibomb@users.noreply.github.com>
2021-11-26 08:34:59 -0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,91 @@
+//define variables
+let termDiv;
+let definitionDiv;
+let audioDiv;
+let originDiv;
+let exampleDiv;
+let synonymsDiv;
+let antonymsDiv;
+let partOfSpeechDiv;
+let phoneticDiv;
+let randomWord = "";
+let randomWordURL = 
+"https://random-word-api.herokuapp.com/word?number=1";
+let wordDefinitionURL = 
+"https://api.dictionaryapi.dev/api/v2/entries/en/";
+let i;
+
+window.onload = function() {
+	termDiv = document.getElementById("term");
+	definitionDiv = document.getElementById("definition");
+	audioDiv = document.getElementById("audio");
+	originDiv = document.getElementById("origin");
+	exampleDiv = document.getElementById("example");
+	partOfSpeechDiv = document.getElementById("partOfSpeech");
+	phoneticDiv = document.getElementById("phonetic");
+	synonymsDiv = document.getElementById("synonyms");
+	antonymsDiv = document.getElementById("antonyms");
+	fetchTerm(randomWordURL);
+};
+
+
+
+function fetchTerm(randomWordURL) {
+	fetch(randomWordURL)
+	.then(response => response.json())
+	.then(data => updateWord(data[0]))
+	.then(word => fetch(wordDefinitionURL + word))
+	.then(response => response.json())
+	.then(data2 => updateDefinition(data2)
+	);
+	
+}//fetchTerm
+
+
+function updateWord(newWord) {
+	randomWord = newWord;
+	termDiv.innerHTML = randomWord;
+	return randomWord;
+}//updateword
+
+function updateDefinition(data2){
+	console.log(data2);
+	
+	//show definition
+	for (let i = 0; i < 100; i++) {
+		if (data2[0] != undefined){
+	        definitionDiv.innerHTML = data2[0].meanings[i].definitions[i].definition;
+	    }
+	}
+	
+	//show audio file
+	if(data2[0].phonetics[0].audio != undefined){
+		let output = "<audio controls><source src='";
+		output += "http:" + data2[0].phonetics[0].audio;
+		output += "' type='audio/mpeg'>";
+		output += "<br>Your browser does not support the audio tag.";
+		output += "</audio>";
+		audioDiv.innerHTML = output;
+	}
+	//show origin
+	if (data2[0].origin != undefined){
+		originDiv.innerHTML = "The word's origins: <br>" + data2[0].origin;
+	}
+	if (data2[0].meanings[0].definitions[0].example != undefined){
+		exampleDiv.innerHTML = data2[0].meanings[0].definitions[0].example;
+	}
+	if (data2[0].meanings[0].partOfSpeech != undefined){
+		partOfSpeechDiv.innerHTML = data2[0].meanings[0].partOfSpeech;
+	}
+	if (data2[0].meanings[0].definitions[0].synonyms != undefined){
+		synonymsDiv.innerHTML = "synonyms:<br>" + data2[0].meanings[0].definitions[0].synonyms;
+	}
+	if (data2[0].meanings[0].definitions[0].antonyms != undefined){
+		antonymsDiv.innerHTML = "Antonyms:<br>" + data2[0].meanings[0].definitions[0].antonyms;
+	}
+	if (data2[0].phonetic != undefined){
+		phoneticDiv.innerHTML = data2[0].phonetic;
+	} else {
+		definitionDiv.innerHTML = data2.message + "<br>Please reload page.";
+	}
+}
